2e230ce7470c0ea37323ce963622b805df332856
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-12-04 05:14:36 +0100

    Hot Fix Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@
 def divisible_by_2(my_list=[]):
     new_list = []
 
-    for p in my_list:
-        if p % 2 == 0:
+    for i in my_list:
+        if i % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
